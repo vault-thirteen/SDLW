5b774cf5e054130cb0d7e2ddbc0f1fc8f91a6605
Vault Thirteen <54213347+vault-thirteen@users.noreply.github.com>
2023-04-18 06:34:35 +0300

    ...

--- SDL/SDL_audio.go
+++ SDL/SDL_audio.go
@@ -20,16 +20,25 @@ const (
 
 // Audio format flags.
 const (
-	AUDIO_U8     = 0x0008
-	AUDIO_S8     = 0x8008
-	AUDIO_U16LSB = 0x0010
+	AUDIO_S8 = 0x8008
+	AUDIO_U8 = 0x0008
+
 	AUDIO_S16LSB = 0x8010
-	AUDIO_U16MSB = 0x1010
+	AUDIO_U16LSB = 0x0010
 	AUDIO_S16MSB = 0x9010
+	AUDIO_U16MSB = 0x1010
+	AUDIO_S16    = AUDIO_S16LSB
+	AUDIO_U16    = AUDIO_U16LSB
+
+	// Integer Samples.
 	AUDIO_S32LSB = 0x8020
 	AUDIO_S32MSB = 0x9020
+	AUDIO_S32    = AUDIO_S32LSB
+
+	// Float Samples.
 	AUDIO_F32LSB = 0x8120
 	AUDIO_F32MSB = 0x9120
+	AUDIO_F32    = AUDIO_F32LSB
 )
 
 // Which audio format changes are allowed when opening a device.
